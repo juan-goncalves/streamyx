d9ef4aac5852cc577dcf647b0838705977a6cab6
Juan Goncalves <juan.goncalves@hey.com>
2024-02-28 18:33:43 +0100

    Cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
-    id("com.google.devtools.ksp") version "1.9.21-1.0.15"
 }
 
 android {
@@ -23,6 +22,10 @@ android {
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = true
+        }
+
         release {
             isMinifyEnabled = false
             proguardFiles(
@@ -31,19 +34,24 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -52,9 +60,7 @@ android {
 }
 
 dependencies {
-
     implementation(project(":dock"))
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -65,17 +71,6 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.bumble.appyx:appyx-navigation:2.0.0-alpha10")
-    implementation("com.bumble.appyx:appyx-interactions-android:2.0.0-alpha10")
     implementation("com.bumble.appyx:backstack-android:2.0.0-alpha10")
     implementation("com.bumble.appyx:spotlight-android:2.0.0-alpha10")
-
-    ksp("com.bumble.appyx:appyx-processor:2.0.0-alpha10")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
